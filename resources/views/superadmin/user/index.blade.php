4142f82d7d3595fcbd574a406d34744431f6c0f3
Johanes Rafael <yohanesrafael80@gmail.com>
2025-12-04 18:14:53 +0700

    membuat halaman index

--- resources/views/superadmin/user/index.blade.php
+++ resources/views/superadmin/user/index.blade.php
@@ -1,1 +1,9 @@
-@livewire('superadmin.user.index')
+@extends('layout.app')
+
+@section('title','Data User')
+@section('menuSuperadminUser','active')
+
+@section('content')
+    @livewire('superadmin.user.index')
+@endsection
+{{-- @livewire('superadmin.user.index') --}}
